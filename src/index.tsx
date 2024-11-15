f6e60ae4dc876d351425c3e3bd15b5103a820180
tacserus <115652334+tacserus@users.noreply.github.com>
2024-11-15 10:48:54 +0500

    module6-task1 fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './react/app.tsx';
-import {offers} from './mocks/offers.ts';
 import {detailedOffers} from './mocks/detailedOffers.ts';
 import {reviews} from './mocks/reviews.ts';
 import {cities} from './mocks/cities.ts';
-import {Provider} from "react-redux";
-import {store} from "./store";
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App offers={ offers } detailedOffers={detailedOffers} comments={reviews} cities={cities}/>
+      <App detailedOffers={detailedOffers} comments={reviews} cities={cities}/>
     </Provider>
   </React.StrictMode>
 );
