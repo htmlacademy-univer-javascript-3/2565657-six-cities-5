7263319f9b4c79bdac4b4dedb5ea29738348ed50
tacserus <115652334+tacserus@users.noreply.github.com>
2024-12-10 00:39:15 +0500

    module7-task2 is done

--- src/react/pages/favorites-page/favorites-page.tsx
+++ src/react/pages/favorites-page/favorites-page.tsx
@@ -6,26 +6,30 @@ import {useAppDispatch} from '../../../store';
 import {useEffect, useState} from 'react';
 import {ApiRouter} from '../../../api/api-router.ts';
 import {setError} from '../../../store/actions.ts';
+import {getToken} from '../../../api/token.ts';
+import {logoutAction} from "../../../store/api-actions.ts";
 
 function FavoritesPage() {
   const [favoriteOffers, setFavoritesOffers] = useState<Offer[]>([]);
-  const [isReceived, setIsReceived] = useState<boolean>(false);
+
   const dispatch = useAppDispatch();
+  const token = getToken();
 
   useEffect(() => {
-    if (!isReceived) {
-      fetch(ApiRouter.Favorites, {
-        method: 'GET'
-      })
-        .then((response) => response.json())
-        .then((data: Offer[]) => setFavoritesOffers(data))
-        .catch((err: string) =>
-          dispatch(setError(err))
-        );
+    const path = `${ApiRouter.BasePath}${ApiRouter.Favorites}`;
 
-      setIsReceived(true);
-    }
-  }, [isReceived]);
+    fetch(path, {
+      method: 'GET',
+      headers: {
+        'x-token': `${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data: Offer[]) => setFavoritesOffers(data))
+      .catch((err: string) =>
+        dispatch(setError(err))
+      );
+  }, []);
 
   const groupedByPlace = favoriteOffers.reduce<Record<string, Offer[]>>(
     (acc, favoriteOffer) => {
@@ -37,74 +41,68 @@ function FavoritesPage() {
     }, {});
 
   return (
-    <body>
-      <div className="page">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <div className="header__left">
-                <Link className="header__logo-link" to={AppRouter.Main}>
-                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
-                </Link>
-              </div>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                      <span className="header__favorite-count">3</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Link className="header__logo-link" to={AppRouter.Main}>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
+              </Link>
             </div>
-          </div>
-        </header>
-
-        <main className="page__main page__main--favorites">
-          <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">Saved listing</h1>
-              <ul className="favorites__list">
-                {Object.keys(groupedByPlace).map((cityName) => (
-                  <li className="favorites__locations-items" key={cityName}>
-                    <div className="favorites__locations locations locations--current">
-                      <div className="locations__item">
-                        <a className="locations__item-link" href="#">
-                          <span>{cityName}</span>
-                        </a>
-                      </div>
-                    </div>
-                    <div className="favorites__places">
-                      {favoriteOffers
-                        .filter((favoriteOffer) => favoriteOffer.city.name === cityName)
-                        .map((favoriteOffer) =>
-                          (
-                            <Link key={favoriteOffer.id} to={ `/offer/${favoriteOffer.id}` }>
-                              <FavoriteCard favoriteOffer={favoriteOffer} key={favoriteOffer.id}/>
-                            </Link>
-                          ))}
-                    </div>
-                  </li>
-                ))}
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item">
+                  <Link
+                    onClick={() => dispatch(logoutAction())}
+                    className="header__nav-link"
+                    to={AppRouter.Main}
+                  >
+                    <span className="header__signout">Sign out</span>
+                  </Link>
+                </li>
               </ul>
-            </section>
+            </nav>
           </div>
-        </main>
-        <footer className="footer container">
-          <a className="footer__logo-link" href="main.html">
-            <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"></img>
-          </a>
-        </footer>
-      </div>
-    </body>
+        </div>
+      </header>
+
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {Object.keys(groupedByPlace).map((cityName) => (
+                <li className="favorites__locations-items" key={cityName}>
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>{cityName}</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="favorites__places">
+                    {favoriteOffers
+                      .filter((favoriteOffer) => favoriteOffer.city.name === cityName)
+                      .map((favoriteOffer) =>
+                        (
+                          <Link key={favoriteOffer.id} to={ `/offer/${favoriteOffer.id}` }>
+                            <FavoriteCard favoriteOffer={favoriteOffer} key={favoriteOffer.id}/>
+                          </Link>
+                        ))}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </main>
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"></img>
+        </a>
+      </footer>
+    </div>
   );
 }
 
--- src/react/pages/main-page/main-page.tsx
+++ src/react/pages/main-page/main-page.tsx
@@ -70,12 +70,13 @@ function MainPage() {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
+                  {authorizationStatus === AuthorizationStatus.Auth &&
+                    <Link className="header__nav-link header__nav-link--profile" to={AppRouter.Favorites}>
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      <span className="header__favorite-count">3</span>
+                    </Link>}
                 </li>
                 <li className="header__nav-item">
                   {authorizationStatus !== AuthorizationStatus.Auth
